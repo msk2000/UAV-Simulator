a456e9392e889d67954a9bbf87cdaf8f3d018817
Safat <your_username@users.noreply.github.com>
2024-10-08 15:04:54 -0400

    formatting

--- simulator.cpp
+++ simulator.cpp
@@ -24,46 +24,41 @@ namespace plt = matplotlibcpp;
 
 int main() 
 {
-
+    // Simulation parameters
     int steps = 10;  
     double dt = 0.00001;
     int vehicle_count = 1;
     std::string fname = "../data.txt";
+
+    //Instantiate a UAV
     Aircraft obj(fname,vehicle_count);
     Aircraft::State state = obj.get_state(); 
     obj.steps = steps;
     obj.dt = dt;
     
-    
+    // Initialize UAV geometry
     obj.initializeVertices();
-
-
     obj.initializePreviousState();
-
-    
-
     obj.initializeVerticesIndices();
     
-    // initialize Easy3D.
     
+    // initialize Easy3D.
     easy3d::initialize(true);
     std::cout << "INITIALISING 3D" <<"\n";
-    // Create the default Easy3D viewer.
-    // Note: a viewer must be created before creating any drawables.
+    // Create the default Easy3D viewer. Note: a viewer must be created before creating any drawables.
+    
     easy3d::Viewer viewer("UAV Simulator");
     std::cout << "Viewer Object created" <<"\n";
 
+    // Draw the aircraft and 3D graphs
     obj.createAircraftDrawable(viewer);
     obj.createGridDrawable(viewer);
 
-    //===========================================================
-
-     // Make sure everything is within the visible region of the viewer.
+    // Make sure everything is within the visible region of the viewer.
     viewer.fit_screen();
-    viewer.set_animation(true);
-    // Define an animation function to specify how vertex positions are updated.
-    // Adapted from example (Tutorial 311)
+    viewer.set_animation(true); // -> True here will create a log file in /build
     
+    // Animation function
     viewer.animation_func_ = [&](easy3d::Viewer* v) -> bool 
     {
         return obj.animate(v, state, dt);
